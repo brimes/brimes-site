a1baef6b6168da28cc3f1227740e30bbc800fdfb
Bruno Lima <bruno@fidelize.com.br>
2014-04-26 14:36:34 -0300

    criando verificacao de usuario em conta

--- app/models/conta.rb
+++ app/models/conta.rb
@@ -12,10 +12,15 @@ class Conta
   field :status, type: Integer
   field :ult_atualizacao, type: String
   
+  scope :do_usuario, ->(user) { where(:usuario => user) }
+  
   def self.save_dados (j_contas, id_usuario)
     contas_salvas = [];
     JSON[j_contas].each do |o_conta|
-      conta = Conta.new
+      conta = Conta.do_usuario(id_usuario).where(:p_id => o_conta["p_id"]).first()
+      if !conta
+        conta = Conta.new
+      end
       conta.usuario = id_usuario
       conta.attributes = o_conta
       if conta.save
